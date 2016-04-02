c25c4a82a1c3c3871e6b96b5a123e231f600cfa6
toadhats <limule@icloud.com>
2016-04-03 00:31:55 +1100

    built webjob skeleton

--- Project/Where2Next/DataDigester/Program.cs
+++ Project/Where2Next/DataDigester/Program.cs
@@ -20,7 +20,7 @@ namespace DataDigester
             var host = new JobHost();
             // The following code will invoke a function called ManualTrigger and 
             // pass in data (value in this case) to the function
-            host.Call(typeof(Functions).GetMethod("ManualTrigger"), new { value = 20 });
+            host.Call(typeof(Functions).GetMethod("UpdateData"));
         }
     }
 }
