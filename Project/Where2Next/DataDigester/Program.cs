2eb2a0cdba96c970a8fcac4691544b1bb76f10d8
toadhats <limule@icloud.com>
2016-04-02 23:16:11 +1100

    created service entity class

--- Project/Where2Next/DataDigester/Program.cs
+++ Project/Where2Next/DataDigester/Program.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Table;
 
 namespace DataDigester
 {
