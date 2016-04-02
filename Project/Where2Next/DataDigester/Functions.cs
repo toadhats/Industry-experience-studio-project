58c668b7a58f90e5cfa543924f5997203fe94d99
toadhats <limule@icloud.com>
2016-04-03 00:49:33 +1100

    Webjob is working

--- Project/Where2Next/DataDigester/Functions.cs
+++ Project/Where2Next/DataDigester/Functions.cs
@@ -1,19 +1,25 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Table;
 
 namespace DataDigester
 {
     public class Functions
     {
         [NoAutomaticTrigger]
-        public static void UpdateData()
+        public static void UpdateData([Table("Services")] IQueryable<Service> tableBinding)
         {
             // Job code goes here
             
+
+
         }
     }
 }
