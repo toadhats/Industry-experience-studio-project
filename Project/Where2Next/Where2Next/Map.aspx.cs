67500f2acb6fc3b63698a2bc224d6352cc15af07
xurongda <rxu25@student.monash.edu>
2016-04-03 14:43:46 +1000

    Add view map functions
    Add view map functions

--- Project/Where2Next/Where2Next/Map.aspx.cs
+++ Project/Where2Next/Where2Next/Map.aspx.cs
@@ -14,29 +14,7 @@ namespace Where2Next
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            if (!IsPostBack)
-            {
-                GLatLng mainLocation = new GLatLng(-38.030, 145.310);
-                GMap1.setCenter(mainLocation, 10);
-                XPinLetter xpinLetter = new XPinLetter(PinShapes.pin_star, "H", Color.Blue, Color.White, Color.Chocolate);
-                GMap1.Add(new GMarker(mainLocation, new GMarkerOptions(new GIcon(xpinLetter.ToString(), xpinLetter.Shadow()))));
-
-                List<SuburbMaster> suburbs = new List<SuburbMaster>();
-                using (suburbEntities dc = new suburbEntities())
-                {
-                    suburbs = dc.SuburbMaster.Where(a => a.State.Equals("VIC")).ToList();
-
-                }
-
-                PinIcon p;
-                GMarker gm;
-                foreach (var i in suburbs)
-                {
-                    p = new PinIcon(PinIcons.home, Color.Cyan);
-                    gm = new GMarker(new GLatLng(Convert.ToDouble(i.LocLa), Convert.ToDouble(i.LocLong)),
-                        new GMarkerOptions(new GIcon(p.ToString(), p.Shadow())));
-                }
-            }
+ 
         }
     }
 }
