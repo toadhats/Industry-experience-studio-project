fd1d93c2f7a4c42f879e39bf48c25a9d6efcc809
toadhats <limule@icloud.com>
2016-04-05 00:18:38 +1000

    we will need this

---
+++ Project/Where2Next/Where2Next/Service.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace DataDigester
+{
+    public class Service : TableEntity
+    {
+        public Service() { }
+        public string serviceType { get; set; }
+        public string serviceName { get; set; }
+        public string address { get; set; }
+        public string suburb { get; set; }
+        public string state { get; set; }
+        public string postcode { get; set; }
+        public string latitude { get; set; }
+        public string longitude { get; set; }
+    }
+}
