2ab460eb5c19b357947c8aaad72336680e20766e
xurongda <rxu25@student.monash.edu>
2016-04-24 21:39:33 +1000

    map bug

--- Project/Where2Next/Where2Next/Mapfunction.aspx.cs
+++ Project/Where2Next/Where2Next/Mapfunction.aspx.cs
@@ -117,15 +117,15 @@ namespace Where2Next
                         {
                             if (System.Text.RegularExpressions.Regex.IsMatch(TextBox1.Text.Trim(), "^\\d+$"))
                             {
-                                query = query + "select NAME,LATITUDE,LONGITUDE,address,icon from " + box.ID + " where postcode='" + TextBox1.Text + "' and LATITUDE !=0 and LATITUDE is not null union ";
+                                query = query + "select NAME,LATITUDE,LONGITUDE,address,icon,CONCAT(suburb,id) as marker from " + box.ID + " where postcode='" + TextBox1.Text + "' and LATITUDE !=0 and LATITUDE is not null union ";
 
                             }
                             else if (System.Text.RegularExpressions.Regex.IsMatch(TextBox1.Text.Trim(), "^\\w+$"))
                             {
-                                query = query + "select NAME,LATITUDE,LONGITUDE,address,icon from " + box.ID + " where suburb='" + TextBox1.Text + "' and LATITUDE !=0 and LATITUDE is not null union ";
+                                query = query + "select NAME,LATITUDE,LONGITUDE,address,icon,CONCAT(suburb,id) as marker from " + box.ID + " where suburb='" + TextBox1.Text + "' and LATITUDE !=0 and LATITUDE is not null union ";
                             } else
                             {
-                                query = query + "select NAME,LATITUDE,LONGITUDE,address,icon from " + box.ID + " and LATITUDE !=0 and LATITUDE is not null union ";
+                                query = query + "select NAME,LATITUDE,LONGITUDE,address,icon,CONCAT(suburb,id) as marker from " + box.ID + " and LATITUDE !=0 and LATITUDE is not null union ";
                             }
 
                         }
@@ -141,6 +141,7 @@ namespace Where2Next
                     string Locations = "";
                     string address = "";
                     string icon = "";
+                    string marker = "";
                     cn.Open();
                     MySqlCommand mcd = new MySqlCommand(query, cn);
                     MySqlDataReader mdr = mcd.ExecuteReader();
@@ -154,7 +155,8 @@ namespace Where2Next
                             NAME = mdr.GetString(0);
                             address = mdr.GetString(3);
                             icon = mdr.GetString(4);
-                            Locations += Environment.NewLine + " var suburb = new google.maps.LatLng(" + Latitude + ", " + Longitude + ");var marker = new google.maps.Marker({position: suburb,icon: '" + icon + "'});marker.setMap(map);var infowindow = new google.maps.InfoWindow({content:'" + NAME + "'});infowindow.open(map,marker);google.maps.event.addListener(marker, 'click', function () {map.setZoom(16);map.setCenter(marker.getPosition());infowindow.open(map, marker);});";
+                            marker = mdr.GetString(5);
+                            Locations += Environment.NewLine + " var suburb = new google.maps.LatLng(" + Latitude + ", " + Longitude + ");var " + marker + " = new google.maps.Marker({position: suburb,icon: '" + icon + "'});" + marker + ".setMap(map);var infowindow = new google.maps.InfoWindow({content:'" + NAME + "'});infowindow.open(map," + marker + "); google.maps.event.addListener(" + marker + ", 'click', function () {map.setZoom(16);map.setCenter(" + marker + ".getPosition());});";
                             js.Text = "<script type='text/javascript'>" +
                  "var myCenter = new google.maps.LatLng(" + Latitude + "," + Longitude + "); function initialize(){var mapProp = {center:myCenter,zoom:13,mapTypeId:google.maps.MapTypeId.ROADMAP};var map=new google.maps.Map(document.getElementById('map_canvas'),mapProp);" + Locations + @" }google.maps.event.addDomListener(window, 'load', initialize);
          </script> ";
