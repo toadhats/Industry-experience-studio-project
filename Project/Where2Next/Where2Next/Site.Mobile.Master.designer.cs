0c3ac5617ddceebf60add214b0c2d75c05a86369
toadhats <limule@icloud.com>
2016-05-09 23:53:30 +1000

    fix missing placeholder in mobile master header

--- Project/Where2Next/Where2Next/Site.Mobile.Master.designer.cs
+++ Project/Where2Next/Where2Next/Site.Mobile.Master.designer.cs
@@ -13,13 +13,13 @@ namespace Where2Next {
     public partial class Site_Mobile {
         
         /// <summary>
-        /// HeadContent control.
+        /// extraHeadContent control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder HeadContent;
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder extraHeadContent;
         
         /// <summary>
         /// MainContent control.
