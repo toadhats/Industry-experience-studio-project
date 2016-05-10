fd614a2e06d89329001214620733a04c15875109
toadhats <limule@icloud.com>
2016-05-11 00:32:50 +1000

    started using the nil coalescing operator to gracefully deal with nulls

---
+++ Project/Where2Next/SqlQuery_1.sql
@@ -0,0 +1,3 @@
+﻿select top 1 Suburb, len(Suburb)
+from where2next.suburb_gnaf
+order by len(Suburb) desc
--- Project/Where2Next/Where2Next/DBConnect.cs
+++ Project/Where2Next/Where2Next/DBConnect.cs
@@ -130,7 +130,7 @@ namespace Where2Next
             queryBuilder.Append("SELECT DISTINCT c.tablename, c.table_displayName FROM where2next.categories c WHERE c.category IN (");
             foreach (var cat in categories)
             {
-                queryBuilder.Append(String.Format("'{0}', ", cat));
+                queryBuilder.Append(string.Format("'{0}', ", cat));
             }
             queryBuilder.Append("'none');"); // Partly a hack to deal with the trailing comma, partly in case I really do end up with a "none" category at any point
 
@@ -162,8 +162,9 @@ namespace Where2Next
             {
                 OpenConnection();
             }
-            SqlCommand cmd = new SqlCommand(null, connection);
-            cmd.CommandText = "SELECT s.suburb, s.postcode, s.latitude, s.longitude, s.wikiURL, s.picURL FROM where2next.suburb_gnaf s WHERE s.suburb = @suburbname";
+            // Using a prepared statement now to mitigate the gaping security hole that is sending
+            // user input to the database
+            SqlCommand cmd = new SqlCommand("SELECT s.suburb, s.postcode, s.latitude, s.longitude, s.wikiURL, s.picURL FROM where2next.suburb_gnaf s WHERE s.suburb = @suburbname", connection); // much better to do this in a single line
             SqlParameter snameParam = new SqlParameter("@suburbname", SqlDbType.VarChar, 40); // Limit to 40 chars because we don't need more, excessive capabilities tend to present risks
             snameParam.IsNullable = false;
             snameParam.Value = suburbName;
@@ -217,28 +218,32 @@ namespace Where2Next
             List<Service> services = new List<Service>();
             foreach (string tableName in tableNames)
             {
-                string tq = String.Format("SELECT s.name, s.address, s.latitude, s.longitude, s.telephone, s.type, s.postcode from where2next.{0} s where s.suburb = '{1}'", tableName, suburbName);
-                SqlCommand getServiceCmd = new SqlCommand();
-                getServiceCmd.CommandText = tq;
-                getServiceCmd.Connection = connection;
+                SqlCommand getServiceCmd = new SqlCommand(string.Format("SELECT s.name, s.address, s.latitude, s.longitude, s.telephone, s.type, s.postcode from where2next.{0} s where s.suburb = @suburbname", tableName), connection);
+                SqlParameter snameParam = new SqlParameter("@suburbname", SqlDbType.VarChar, 40);
+                snameParam.IsNullable = false;
+                snameParam.Value = suburbName;
+                getServiceCmd.Parameters.Add(snameParam);
+                getServiceCmd.Prepare();
+
                 var dr = getServiceCmd.ExecuteReader(); // We're NOT setting up to close the connection here because we'll need to use it bunch of times\
                 // Hopefully we're getting rows from a given table now...
                 if (dr.HasRows)
                 {
                     while (dr.Read())
-                    {
-                        var sname = dr.GetString(0);
-                        var saddress = dr.GetString(1);
-                        var slatitude = "0";// dr.GetDouble(2).ToString(); // TODO: Fix this. Make it null tolerant
-                        var slongitude = "0";// dr.GetDouble(3).ToString();
-                        var stelephone = "0"; //dr.GetString(4); //TODO: Handle nulls!
-                        var stype = dr.GetString(5);
-                        var spostcode = dr.GetInt32(6).ToString();
+                    {// Discovered the nil coalescing operator.
+                        var stype = dr["type"] as string ?? "—"; // Moved this here so we can use it to generate a service name if we get a null
+                        var sname = dr["name"] as string ?? string.Format("{0} {1}", suburbName, stype); // If the name field of the service is empty, then invent a name based on suburb name and service type.
+                        var saddress = dr["address"] as string ?? "—";
+                        var slatitude = string.Format("{0}", dr["latitude"] as double? ?? default(double)); // Not sure this is the most elegant way of doing this
+                        var slongitude = string.Format("{0}", dr["longitude"] as double? ?? default(double));
+                        var stelephone = dr["telephone"] as string ?? "0";
+
+                        var spostcode = string.Format("{0}", dr["postcode"] as Int32? ?? 0); // I keep wanting to use |> to do this but I can't...
                         Service s = new Service(stype, sname, saddress, suburbName, "VIC", spostcode, slatitude, slongitude, stelephone);
                         services.Add(s);
                     }
                 }
-                dr.Close();
+                dr.Close(); // Double checking I closed this...
             }
             return services;
         }
